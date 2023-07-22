c5e78779285b175e21c3001c7f357d910dbb977a
Lawrence Ma <lawrencema415@gmail.com>
2023-07-22 13:59:23 -0700

    Set up Form

--- utils/database.js
+++ utils/database.js
@@ -3,23 +3,24 @@ import mongoose from 'mongoose';
 let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true)
+  mongoose.set('strictQuery', true);
 
-  if (isConnected) {
-    console.log('mongodb is already connected')
+  if(isConnected) {
+    console.log('MongoDB is already connected');
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'share_prompt',
+      dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
 
     isConnected = true;
-    console.log('MongoDB connected');
+
+    console.log('MongoDB connected')
   } catch (error) {
-    console.log('error', error);
+    console.log(error);
   }
 }
